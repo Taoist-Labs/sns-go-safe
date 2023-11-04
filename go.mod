5ae2591c1e0397ec61f127d7fd3fcb05d33d2c3e
xiaosongfu <xiaosong.fu@outlook.com>
2023-11-04 20:37:16 +0800

    add basic code

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Taoist-Labs/sns-go-safe
+
+go 1.21
+
+require (
+	github.com/Taoist-Labs/sns-go-api v0.1.0
+)
---
+++ safe.go
@@ -0,0 +1,18 @@
+package sns_go_safe
+
+import api "github.com/Taoist-Labs/sns-go-api"
+
+func IsSafe(name string) bool {
+	return api.IsSafe(name)
+}
+func Safe(name []string) []string {
+	return api.Safe(name)
+}
+
+func IsAvailable(name string) bool {
+	return api.IsAvailable(name)
+}
+
+func Available(name []string) []string {
+	return api.Available(name)
+}
