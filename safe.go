db54c573c5cf30ac05e82fae046e32fbbb975e6a
xiaosongfu <xiaosong.fu@outlook.com>
2023-11-05 22:57:46 +0800

    fix: add methods

--- safe.go
+++ safe.go
@@ -1,19 +1,18 @@
 package sns_go_safe
 
-import _ "github.com/Taoist-Labs/sns-go-api"
+import api "github.com/Taoist-Labs/sns-go-api"
 
-//
-//func IsSafe(name string) bool {
-//	return api.IsSafe(name)
-//}
-//func Safe(name []string) []string {
-//	return api.Safe(name)
-//}
-//
-//func IsAvailable(name string) bool {
-//	return api.IsAvailable(name)
-//}
-//
-//func Available(name []string) []string {
-//	return api.Available(name)
-//}
+func IsSafe(name, apiUrl string) bool {
+	return api.IsSafe(name, apiUrl)
+}
+func Safe(name []string, apiUrl string) []string {
+	return api.Safe(name, apiUrl)
+}
+
+func IsAvailable(name, apiUrl string) bool {
+	return api.IsAvailable(name, apiUrl)
+}
+
+func Available(name []string, apiUrl string) []string {
+	return api.Available(name, apiUrl)
+}
